6322b03329a7641e799c090ff53d0b9ef279f257
davincif <LDaVinciF@gmail.com>
2016-05-27 20:42:56 -0300

    preparing ground to crosscompilation

--- NLF_SCR/configure.sh
+++ NLF_SCR/configure.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-DS="$" #Dolar Sign
+###FUNCTIONS###
+config_help() {
+	echo "configure file help
+You need to provide a compiling flag to this configure file. As below
 
-printf "#the compiler
+-l2l or --linux2linux
+	configure a Makefile to be built in linux OS
+-l2w or --linux2windows
+	configure a Makefile for linux to be built in Windows OS"
+}
+
+make_for_linux() {
+	echo "Makefile for linux"
+	printf "#the compiler
 CC = gcc
 
 #the standart ompilation flags of the project
@@ -11,51 +22,74 @@ CFLAGS = -O3 -Wall -Wno-unused-variable -Wno-unused-but-set-variable -Wno-implic
 #path to the folder's root, where the holy not build framework is. Relate to this Makefile
 PREPATH = ..\n" > Makefile
 
-printf "
+		printf "
 #Path to the SDL, SDL_image and Lua includes and libs
 SDL2INCLUDES = -I $DS(PREPATH)/SDL2/include
 SDL2LIBS =" >> Makefile
+	for f in ../SDL2/lib/*.a; do
+		if ! [[ $f == *"_image"* ]]
+		then
+			printf " $f" >> Makefile
+		fi
+	done
+	for f in ../SDL2/lib/*.so.*; do
+		if ! [[ $f == *"_image"* ]]
+		then
+			printf " $f" >> Makefile
+		fi
+	done
 
-for f in ../SDL2/lib/*.a
-do
-	if ! [[ $f == *"_image"* ]]
-	then
-		printf " $f" >> Makefile
-	fi
-done
-for f in ../SDL2/lib/*.so.*
-do
-	if ! [[ $f == *"_image"* ]]
-	then
-		printf " $f" >> Makefile
-	fi
-done
-
-printf "
+	printf "
 SDLIMAGE2INCLUDES = -I $DS(PREPATH)/SDL2/SDL_image
 SDLIMAGE2LIBS =" >> Makefile
 
-for f in ../SDL2/lib/*.a
-do
-	if [[ $f == *"_image"* ]]
-	then
-		printf " $f" >> Makefile
-	fi
-done
-for f in ../SDL2/lib/*.so.*
-do
-	if [[ $f == *"_image"* ]]
-	then
-		printf " $f" >> Makefile
-	fi
-done
+	for f in ../SDL2/lib/*.a; do
+		if [[ $f == *"_image"* ]]
+		then
+			printf " $f" >> Makefile
+		fi
+	done
+	for f in ../SDL2/lib/*.so.*; do
+		if [[ $f == *"_image"* ]]
+		then
+			printf " $f" >> Makefile
+		fi
+	done
 
-printf "
+	printf "
 #LUAINCLUDES = -I $DS(PREPATH)/lua/
 #LUALIB = $DS(PREPATH)/lua/ -llua -lm" >> Makefile
 
-printf "\n
+	printf "\n
 #Where to put the compiled program
 COMPILEPATH = $DS(PREPATH)/BINARIES/\n" >> Makefile
 
-cat make_config >> Makefile
+	cat make_config >> Makefile
+}
+###############
+
+DS="$" #Dolar Sign
+
+if [ -z "$1" ]; then
+	config_help
+	exit
+fi
+
+for arg in "$@"; do
+	case "$@" in
+	-l2l)
+	--linux2linux)
+		make_for_linux
+	;;
+
+	-l2w)
+	--linux2windows)
+		echo "pera vissi"
+	;;
+
+	*)
+		echo "Not recognized option"
+		config_help
+	;;
+	esac
+done
